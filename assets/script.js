fa2ad09758bb26de821d6aab2ba9e7237cc46074
Palatta <alib.shanti@gmail.com>
2022-06-07 14:46:34 +0200

    script modified

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,68 @@
+function getData() {
+  let conciertos_json = JSON.parse(conciertos);
+  return conciertos_json;
+}
 
+function displayData(data) {
+  let conciertos_html = '';
+  for (var i = 0; i < data.length; i++) {
+    //display elements
+    conciertos_toHTML(data[i]);
+  }
+}
+
+
+
+
+function conciertos_toHTML(concierto) {
+  concierto = '<div class="concierto">';
+  concierto += '<span>titulo: '+titulo+'</span>';
+  concierto += '<span>descr: '+descr+'</span>';
+  concierto += '<span>tipo_musica: '+tipo_musica+'</span>';
+  concierto += '<span>precio: '+precio+'</span>';
+  concierto += '<span>fecha: '+fecha+'</span>';
+  concierto += '<span>lugar: '+lugar+'</span>';
+  concierto += '<span>horario: '+horario+'</span>';
+  concierto += '<span>edad_min: '+edad_min+'</span>';
+  concierto += '<span>plazas: '+plazas+'</span>';
+  concierto += '<span>img: '+img+'</span>';
+  concierto += '</div>';
+  return concierto;
+}
+
+
+document.querySelector('conciertos').innerHTML = conciertos_toHTML[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let conciertos_json = JSON.parse(conciertos); //convierte cadenas JSON en objetos JavaScript
 let lista = "";
 
 for (let i = 0; i < conciertos_json.length;i++){
     lista += "<p>" + "Título: " + conciertos_json[i].titulo + ". Descripción: " + conciertos_json[i].descr + ". Tipo de música: "
-        + conciertos_json[i].tipo_musica + ". Precio: " + conciertos_json[i].precio + "Fecha: " + conciertos_json[i].fecha 
-        + ". Lugar: " + conciertos_json[i].lugar + ". Horario: " + conciertos_json[i].horario + ". Edad mínima: " + conciertos_json[i].edad_min + "Plazas: " + 
+        + conciertos_json[i].tipo_musica + ". Precio: " + conciertos_json[i].precio + "Fecha: " + conciertos_json[i].fecha
+        + ". Lugar: " + conciertos_json[i].lugar + ". Horario: " + conciertos_json[i].horario + ". Edad mínima: " + conciertos_json[i].edad_min + "Plazas: " +
           conciertos_json[i].plazas + "<p>";
 }
 
 document.getElementsByClassName('conciertos')[0].innerHTML = lista;
 
-/*
+
 function concierto_to_html(titulo, descr, tipo_musica, precio, fecha, lugar, horario, edad_min, plazas, img) {
   concierto = '<div class="concierto">';
   concierto += '<span>titulo: '+titulo+'</span>';
