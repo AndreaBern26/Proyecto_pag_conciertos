8660aa28daa0990a6346743b6c532c7746db02bd
Palatta <alib.shanti@gmail.com>
2022-06-08 21:00:19 +0200

    index page script shows all content

--- assets/script.js
+++ assets/script.js
@@ -1,118 +1,41 @@
-function getData() {
-  let conciertos_json = JSON.parse(conciertos);
-  return conciertos_json;
-}
 
-function displayData(data) {
-  let conciertos_html = '';
-  for (var i = 0; i < data.length; i++) {
-    //display elements
-    conciertos_toHTML(data[i]);
-  }
-}
+let concierto = '';
 
+for (var i = 0; i < conciertos.length; i++) {
 
-
-
-function conciertos_toHTML(concierto) {
-  concierto = '<div class="concierto">';
-  concierto += '<span>titulo: '+titulo+'</span>';
-  concierto += '<span>descr: '+descr+'</span>';
-  concierto += '<span>tipo_musica: '+tipo_musica+'</span>';
-  concierto += '<span>precio: '+precio+'</span>';
-  concierto += '<span>fecha: '+fecha+'</span>';
-  concierto += '<span>lugar: '+lugar+'</span>';
-  concierto += '<span>horario: '+horario+'</span>';
-  concierto += '<span>edad_min: '+edad_min+'</span>';
-  concierto += '<span>plazas: '+plazas+'</span>';
-  concierto += '<span>img: '+img+'</span>';
+  concierto += '<div class="concierto">';
+  concierto += '<div class="div-image">';
+  concierto += '<img class="image" src="'+conciertos[i].img+'" alt="'+conciertos[i].titulo+'">';
   concierto += '</div>';
-  return concierto;
-}
-
-
-document.querySelector('conciertos').innerHTML = conciertos_toHTML[0];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let conciertos_json = JSON.parse(conciertos); //convierte cadenas JSON en objetos JavaScript
-let lista = "";
-
-for (let i = 0; i < conciertos_json.length;i++){
-    lista += "<p>" + "Título: " + conciertos_json[i].titulo + ". Descripción: " + conciertos_json[i].descr + ". Tipo de música: "
-        + conciertos_json[i].tipo_musica + ". Precio: " + conciertos_json[i].precio + "Fecha: " + conciertos_json[i].fecha
-        + ". Lugar: " + conciertos_json[i].lugar + ". Horario: " + conciertos_json[i].horario + ". Edad mínima: " + conciertos_json[i].edad_min + "Plazas: " +
-          conciertos_json[i].plazas + "<p>";
-}
-
-document.getElementsByClassName('conciertos')[0].innerHTML = lista;
-
-
-function concierto_to_html(titulo, descr, tipo_musica, precio, fecha, lugar, horario, edad_min, plazas, img) {
-  concierto = '<div class="concierto">';
-  concierto += '<span>titulo: '+titulo+'</span>';
-  concierto += '<span>descr: '+descr+'</span>';
-  concierto += '<span>tipo_musica: '+tipo_musica+'</span>';
-  concierto += '<span>precio: '+precio+'</span>';
-  concierto += '<span>fecha: '+fecha+'</span>';
-  concierto += '<span>lugar: '+lugar+'</span>';
-  concierto += '<span>horario: '+horario+'</span>';
-  concierto += '<span>edad_min: '+edad_min+'</span>';
-  concierto += '<span>plazas: '+plazas+'</span>';
-  concierto += '<span>img: '+img+'</span>';
+  concierto += '<div class="div-titulo">';
+  concierto += '<h3 class="titulo">'+ conciertos[i].titulo +'</h3>';
   concierto += '</div>';
-  return concierto;
-}
-for (i= 0; i < conciertos.length; i++) {
-  titulo = conciertos[i].titulo;
-  descr = conciertos[i].descr;
-  tipo_musica = conciertos[i].tipo_musica;
-  precio = conciertos[i].precio;
-  fecha = conciertos[i].fecha;
-  lugar = conciertos[i].lugar;
-  horario = conciertos[i].horario;
-  edad_min = conciertos[i].edad_min;
-  plazas = conciertos[i].plazas;
-  img = conciertos[i].img;
-  concierto_html = concierto_to_html(titulo, descr, tipo_musica, precio, fecha, lugar, horario, edad_min, plazas, img);
+  concierto += '<div class="div-descripcion">';
+  concierto += '<p class="descripcion"><strong>descripcion:</strong> '+conciertos[i].descr+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-tipo-musica">';
+  concierto += '<p class="tipo-musica"><strong>genero musical:</strong> '+conciertos[i].tipo_musica+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-precio">';
+  concierto += '<p class="precio"><strong>precio:</strong> '+conciertos[i].precio+'€'+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-fecha">';
+  concierto += '<p class="fecha"><strong>fecha:</strong> '+conciertos[i].fecha+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-lugar">';
+  concierto += '<p class="lugar"><strong>lugar:</strong> '+conciertos[i].lugar+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-horario">';
+  concierto += '<p class="horario"><strong>horario:</strong> '+conciertos[i].horario+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-edad-minima">';
+  concierto += '<p class="edad-minima"><strong>edad_min:</strong> '+conciertos[i].edad_min+'</p>';
+  concierto += '</div>';
+  concierto += '<div class="div-plazas">';
+  concierto += '<p class="plazas"><strong>plazas:</strong> '+conciertos[i].plazas+'</p>';
+  concierto += '</div>';
+  concierto += '</div>';
 
 }
 
-
-
-let conc = document.getElementById('concierto');
-conc.innerHTML = concierto_to_html();
-*/
-
-
-
-
-
-/*function buildTable(data) {
-  let table = document.getElementById('tb-concierto');
-  for (let i = 0; i < data.length; i++) {
-    let row = `<tr>
-                <td>`+$(data[i].titulo)+`</td>
-                  <td>`+$(data[i].descr)+`</td>
-                </tr>`;
-    table.innerHTML += row;
-
-  }
-}
-
-  buildTable(conciertos); */
+document.querySelector('article').innerHTML = concierto;
